43dd2c467f8b24abdd695e2b2ee0177df9dd4805
潘柏信 <baixin.pan@mobvista.com>
2024-02-21 17:27:21 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-TestSpecs_Tests/Pods-TestSpecs_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestSpecs_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TestSpecs_Tests
+@end
---
+++ Example/Pods/Target Support Files/TestSpecs/TestSpecs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestSpecs : NSObject
+@end
+@implementation PodsDummy_TestSpecs
+@end
